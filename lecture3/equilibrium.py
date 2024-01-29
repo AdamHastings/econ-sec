2f0ec754b2eba793ef788c8fd1738758a042bbce
adamhastings <itsadamhastings@gmail.com>
2024-01-29 14:27:05 -0500

    supply and demand curves

--- lecture3/equilibrium.py
+++ lecture3/equilibrium.py
@@ -13,26 +13,66 @@ S = np.zeros(1000)
 
 for i,x in enumerate(X):
     if x <= 1:
-        D[i] = 1/x
+        D[i] = 7/x
     elif x <= 3/2:
-        D[i] = 0.5/x
+        D[i] = 5/x
     else:
         D[i] = 0
 
-N = 3
-
+N = 6
 for i,x in enumerate(X):
     if x <= 1:
         S[i] = 0
     else:
         S[i] = N
 
-
-# print(D)
-
+plt.clf()
 plt.plot(X, D, label="demand")
 plt.plot(X, S, label="supply")
 plt.legend()
-plt.ylim(0,5)
-plt.savefig("equilibrium.png")
+plt.xlabel("price")
+plt.ylabel("quantity")
+plt.yticks([0,2,4,6,8,10], ["", "", "", "N", "", ""])
+plt.ylim(0,10)
+plt.title("N > Y2/p")
+plt.savefig("equilibrium1.png")
+
+N = 4
+for i,x in enumerate(X):
+    if x <= 1:
+        S[i] = 0
+    else:
+        S[i] = N
+
+plt.clf()
+plt.plot(X, D, label="demand")
+plt.plot(X, S, label="supply")
+plt.legend()
+plt.xlabel("price")
+plt.ylabel("quantity")
+plt.yticks([0,2,4,6,8,10], ["", "", "N", "", "", ""])
+plt.ylim(0,10)
+plt.title("N = Y2/p")
+plt.savefig("equilibrium2.png")
+
+N = 2
+for i,x in enumerate(X):
+    if x <= 1:
+        S[i] = 0
+    else:
+        S[i] = N
+
+plt.clf()
+plt.plot(X, D, label="demand")
+plt.plot(X, S, label="supply")
+plt.legend()
+plt.xlabel("price")
+plt.ylabel("quantity")
+plt.yticks([0,2,4,6,8,10], ["", "N", "", "", "", ""])
+plt.ylim(0,10)
+plt.title("N < Y2/p")
+plt.savefig("equilibrium3.png")
+
+
+
 
